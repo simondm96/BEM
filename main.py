f2401d130ae5b26f469fc1f544bf66cc257eb33e
Simon De Meester <simon.de.meester@telenet.be>
2018-03-03 20:45:37 +0100

    Updated loading function with description and bugfix for appendCSV

--- main.py
+++ main.py
@@ -9,10 +9,21 @@ About: Main file for a simple BEM code for AE4135
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 def load_polar(filename, appendCSV=True):
+    """
+    Loads a polar from a file with title and headers
+    
+    Input:
+            filename  = str, name of the file which contains the polar
+            AppendCSV = Bool, defines if '.csv' should be added to filename, defaults to True
+          
+    Output:
+            polar     =  numpy array containing AoA, Cl, Cd, Cm
+    """
     if appendCSV:
-        filename += 'csv' 
+        filename += '.csv' 
     polar = np.genfromtxt(filename, delimiter=',', skip_header=2)
     
     return polar
