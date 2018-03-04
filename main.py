bb8de65587d2c0e9aa19df6f075acdcc1389944b
Simon De Meester <simon.de.meester@telenet.be>
2018-03-04 17:16:30 +0100

    Added a mapping function and the twist function

--- main.py
+++ main.py
@@ -11,6 +11,55 @@ About: Main file for a simple BEM code for AE4135
 import numpy as np
 import matplotlib.pyplot as plt
 
+class rotor:
+    """
+    A class which contains properties and a discretisation for a rotor for the 
+    BEM model.
+    
+    Attributes:
+            elements    = ndarray, contains the r-coordinates of the middle 
+                          sections of the blade
+            ends        = ndarray, contains the ends of each section, has length 
+                          len(blades)+1
+            twist       = ndarray, contains the twist of each section in radians
+            pitch       = float, global blade pitch in radians
+            chord       = ndarray, chord distribution of each section in meter
+            num_blades  = float, number of blades
+            
+    """
+    def __init___(self, num_blades, r_in, r_out, twist, chord, N=20):
+        """
+        Initialises the rotor class
+        
+        Input:
+            num_blades  = int, number of blades
+            r_in        = float, starting radius of the blade
+        """
+
+def twist(section, r_start, r_end):
+    """
+    Generates a twist function
+    
+    Input:
+        section     = ndarray, the section(s) for which the  twist should be
+                      calculated
+                      
+    Output:
+        twist       = ndarray, the twist for the section(s). If sections is a
+                      float, returns a float
+    """
+    section_norm = map_values(section,r_start, r_end, 0, 1)
+    twist = 14*(1-section_norm)*np.pi/180.
+    return twist
+
+
+def map_values(data, x_start1, x_end1, x_start2, x_end2):
+    """
+    Maps data with boundaries x_start1 and x_end1 to x_start2 and x_start2
+    """
+    return x_start2 + (data-x_start1)*(x_end1-x_start1)/(x_end2-x_start1)
+
+
 def load_polar(filename, appendCSV=True):
     """
     Loads a polar from a file with title and headers
